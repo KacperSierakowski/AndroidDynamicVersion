85b01f985fc97773a232732e22a728634cbb7393
Kacper Sierakowski <k.sierakowski@luca.eu>
2019-05-29 09:10:59 +0200

    Added button and TextView to show the current version

--- app/src/main/java/com/example/androiddynamicversion/MainActivity.java
+++ app/src/main/java/com/example/androiddynamicversion/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.androiddynamicversion;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        //button.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void BtnOne(View view) {
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+            int versionCode = pInfo.versionCode;
+            Log.d("MyApp", "Version Name : " + version + "\n Version Code : " + versionCode);
+            TextView VerionTextView = (TextView)findViewById(R.id.VerionTextView);
+            VerionTextView.setText("Version Name : " + version + "\n Version Code : " + versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            Log.d("MyApp", "PackageManager Catch : " + e.toString());
+        }
+
     }
 }
